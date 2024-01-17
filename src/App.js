e77eb0093230b586fabd32d9cdf4dc084425c3d2
DinhNhat0810 <dinhnhat0810@gmail.com>
2024-01-17 22:38:01 +0700

    clear import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { publicRoutes, privateRoutes } from '~/routes';
-// import { DefaultLayout } from '~/Layouts';
 import { Fragment } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AuthLayout from 'layouts/auth';
